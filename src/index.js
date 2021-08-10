de58937c1f440bcf50a425e00e80f821f1f658d1
margoroma <margoroma2010@gmail.com>
2021-08-10 10:57:52 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -3,8 +3,74 @@ function eval() {
     return;
 }
 
+
 function expressionCalculator(expr) {
-    // write your solution here
+    if (typeof(expr) === 'string') {
+        arr = (expr.replace(/\s+/g, '')).match(/[+-/*//()]|-?\d+(?:\.\d+)?/g);
+        for (i = 0; i < arr.length; i ++) {
+            if (arr[i] !== '(' && arr[i] !== ')' && arr[i] !== '/' && arr[i] !== '*' && arr[i] !== '-' && arr[i] !== '+') {
+                arr[i] = Number(arr[i]);
+            }        
+        }
+    }
+    let countOpen = arr.filter( i => i === '(');
+    let countClose = arr.filter( i => i === ')');
+    if (countOpen.length !== countClose.length) {
+        throw 'ExpressionError: Brackets must be paired';
+    }
+    else {
+        if (arr.includes('(') === false) {
+            result = operationsMath(arr);
+            return result;
+        }
+        else {
+            indFirst = arr.lastIndexOf('(');
+            arrLast = arr.slice(indFirst);
+            indLast = arrLast.indexOf('\)') + indFirst;
+            deepOperations = (arr.slice(indFirst, indLast + 1)).slice(1, -1);
+            lengthDeep = deepOperations.length + 2;
+            result = operationsMath(deepOperations);
+            arr.splice(indFirst, lengthDeep, result);
+            return expressionCalculator(arr);
+        }
+    } 
+}
+
+function operationsMath(expr) {
+    while (expr.length > 1) {
+        
+        if (expr.includes('/')) {
+            index = expr.indexOf('/');
+            if (expr [index + 1] === 0) {
+                throw 'TypeError: Division by zero.';
+            }
+            else {
+                result = expr[index - 1] / expr [index + 1];
+                expr.splice([index - 1], 3, result);
+                return operationsMath(expr);
+            }
+        }
+        else if (expr.includes('*')) {
+            index = expr.indexOf('*');
+            result = expr[index - 1] * expr [index + 1];
+            expr.splice([index - 1], 3, result);
+            return operationsMath(expr);
+        }
+        else if (expr.includes('-')) {
+            index = expr.indexOf('-');
+            result = expr[index - 1] - expr [index + 1];
+            expr.splice([index - 1], 3, result);
+            return operationsMath(expr);
+        }
+        else if (expr.includes('+')) {
+            index = expr.indexOf('+');
+            result = expr[index - 1] + expr [index + 1];
+            expr.splice([index - 1], 3, result);
+            return operationsMath(expr);
+        }
+    }
+    result = expr[0];
+    return result;
 }
 
 module.exports = {
